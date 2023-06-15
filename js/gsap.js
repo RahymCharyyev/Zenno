13f8cc720f26d7de0e8d9d1eb771037a84a324ea
RahymCharyev <r569837@gmail.com>
2023-06-15 22:27:35 +0500

    ✨feature: Fixed animation

--- js/gsap.js
+++ js/gsap.js
@@ -2,7 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 let sections = gsap.utils.toArray(".zenno__slider-slide");
 
 gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
+  xPercent: -95 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
     trigger: ".zenno__slider-wrapper",
--- js/svg.js
+++ js/svg.js
@@ -1,18 +1,25 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    const animated__logo = entry.target.querySelector(".animated__logo");
-    const path1 = entry.target.querySelector(".path1");
-    const path2 = entry.target.querySelector(".path2");
+    // const path = entry.target.querySelector(".path");
+    // const path1 = entry.target.querySelector(".path1");
+    // const path2 = entry.target.querySelector(".path2");
+    const test1 = entry.target.querySelector(".test1");
+    const test3 = entry.target.querySelector(".test3");
 
     if (entry.isIntersecting) {
-      path1.classList.add("path1-animation");
-      path2.classList.add("path2-animation");
-      return; // if we added the class, exit the function
+      // path.classList.add("path-animation");
+      // path1.classList.add("path1-animation");
+      // path2.classList.add("path2-animation");
+      test1.classList.add("test1-animation");
+      test3.classList.add("test3-animation");
+      return;
     }
 
-    // We're not intersecting, so remove the class and restore opacity!
-    path1.classList.remove("path1-animation");
-    path2.classList.remove("path2-animation");
+    // path.classList.remove("path-animation");
+    // path1.classList.remove("path1-animation");
+    // path2.classList.remove("path2-animation");
+    test1.classList.remove("test1-animation");
+    test3.classList.remove("test3-animation");
   });
 });
 
